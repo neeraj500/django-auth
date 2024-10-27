38f6d2922f8a4d42b77c1cedbed451ca0b202eb7
Neeraj Madake <neerajmadake18@gmail.com>
2024-10-27 12:51:36 +0530

    first commit

---
+++ mainapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+]
