ac2f49d9b05420dc4018d48961be95d462be384f
Neeraj Madake <neerajmadake18@gmail.com>
2024-10-28 21:33:43 +0530

    clear main branch

--- mainapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def dashboard(request): # Main screen
-    return render(request, 'registration/dashboard.html', {'section': 'dashboard'})
-
-
